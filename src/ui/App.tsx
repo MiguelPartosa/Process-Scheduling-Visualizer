6503a7ce0fc6d11f527c13e2f0a2f58453d41353
Miguel Partosa <64950379+MiguelPartosa@users.noreply.github.com>
2024-10-31 17:05:57 +0800

    Styling themes and global/ local css file seperation

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,11 +1,18 @@
 import { useState, useRef } from "react";
 import "./Globals.css";
+import "./App.css";
 
 function App() {
     // const val = window.myAPI;
     return (
         <>
-            <h1>Hello World!</h1>
+            <div className="container">
+                <div className="nav-bar">navbar</div>
+                <div className="job-slider">job slider</div>
+                <div className="job-cycles">jobs input</div>
+                <div className="main-buttons"></div>
+                <h1>Hello World!</h1>
+            </div>
         </>
     );
 }
