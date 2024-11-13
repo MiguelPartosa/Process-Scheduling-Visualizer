116ba625999441edd2d25e308f4fa1093f35e977
Miguel Partosa <64950379+MiguelPartosa@users.noreply.github.com>
2024-11-13 11:57:55 +0800

    Add Icons and hover styling

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -5,32 +5,30 @@ import { isDev } from "./util.js";
 // crashes on build
 // type test = string;
 const createWindow = () => {
-    const mainWindow = new BrowserWindow({
-        width: 500,
-        height: 600,
-        webPreferences: {
-            preload: path.join(app.getAppPath(), "src/electron/preload.cts"),
-        },
-    });
-    if (isDev()) {
-        //allows us to hot reload
-        mainWindow.loadURL("http://localhost:3000");
-    } else {
-        mainWindow.loadFile(
-            path.join(app.getAppPath(), "/dist-react/index.html")
-        );
-    }
+  const mainWindow = new BrowserWindow({
+    width: 500,
+    height: 600,
+    webPreferences: {
+      preload: path.join(app.getAppPath(), "src/electron/preload.cts"),
+    },
+  });
+  if (isDev()) {
+    //allows us to hot reload
+    mainWindow.loadURL("http://localhost:3000");
+  } else {
+    mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
+  }
 };
 
 // Boilerplate to handle different OS platforms
 app.on("ready", () => {
-    createWindow();
+  createWindow();
 
-    app.on("activate", () => {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow();
-    });
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
 });
 
 app.on("window-all-closed", () => {
-    if (process.platform !== "darwin") app.quit();
+  if (process.platform !== "darwin") app.quit();
 });
--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -2,20 +2,47 @@
 // import "./Globals.css";
 import "./App.css";
 import "./index.css";
+import {
+  IconFCFS,
+  IconRoundRobin,
+  IconSRT,
+  IconSJN,
+} from "./assets/icons/NavIcons.tsx";
+
+const NavIconButton = ({ IconComponent, label }) => (
+  <div
+    className="tw-text-gray-500 hover:tw-text-white tw-font-bold hover:tw-scale-125
+               tw-ease-in-out tw-delay-10 tw-duration-500 tw-flex tw-flex-col tw-justify-center tw-w-20"
+  >
+    <IconComponent className="tw-self-center tw-fill-current" />
+    <div className="tw-text-center">{label}</div>
+  </div>
+);
 
 function App() {
-    // const val = window.myAPI;
-    return (
-        <>
-            <div className="container">
-                <div className="tw-nav-bar">navbar</div>
-                <div className="tw-job-slider">job slider</div>
-                <div className="tw-job-cycles">jobs input</div>
-                <div className="tw-main-buttons"></div>
-                <div className="tw-text-3xl tw-font-thin tw-underline">Hello World!</div>
-            </div>
-        </>
-    );
+  // const val = window.myAPI;
+  return (
+    <>
+      <div className="container">
+        <div
+          className="tw-group tw-min-w-300 tw-flex tw-justify-evenly  tw-border tw-w-11/12 tw-py-2 tw-mb-2 
+                hover:tw-bg-neutral-800 tw-rounded-lg hover:tw-shadow-2xl tw-transition-all 
+                tw-ease-in-out tw-delay-10 tw-duration-500"
+        >
+          <NavIconButton IconComponent={IconFCFS} label="FCFS" />
+          <NavIconButton IconComponent={IconSJN} label="SJN" />
+          <NavIconButton IconComponent={IconSRT} label="SRT" />
+          <NavIconButton IconComponent={IconRoundRobin} label="Round Robin" />
+        </div>
+        <div className="job-slider">job slider</div>
+        <div className="job-cycles">jobs input</div>
+        <div className="main-buttons"></div>
+        <div className="tw-text-3xl tw-font-thin tw-underline">
+          Hello World!
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
